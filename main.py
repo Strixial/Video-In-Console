b06a374ec28cdb6dc6a5ca6dcb28ac8fb5b94a08
Strixial <73123605+Strixial@users.noreply.github.com>
2022-08-11 11:40:52 +0100

    stuff

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# coding: utf-8
+
+# Import libraries and download any missing
+import os, sys, time, multiprocessing
+
+os.system("pip install -r requirements.txt")
+import pillow, numpy, ffmpeg, pafy, vlc, cv2
+from image_to_ascii import ImageToAscii
+
+GrayScale = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\"^`". """ # 70 shades of gray
+
+VideoUrl = input("Enter video url: ")
+Video = pafy.new(VideoUrl)
+BestVideo = Video.getbest(preftype="mp4")
+
+Capture = cv2.VideoCapture(BestVideo.url)
 
